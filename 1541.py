8557096da7afa5a88c23e82fd40284cfb7cfec34
Moonsik Song <sikso1892@gmail.com>
2023-07-05 17:39:44 +0900

    boj 1541

---
+++ 1541.py
@@ -0,0 +1,20 @@
+
+import sys
+
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+eq = input()
+nums = eq.split('-')
+acc = 0
+
+lx = sum(map(int, (nums[0].split('+'))))
+if nums[0] == '-':
+    acc -= lx
+else:
+    acc += lx
+
+for rx in nums[1:]:
+    rx = sum(map(int, (rx.split('+'))))
+    acc -= rx
+print(acc)
