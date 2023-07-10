41a4cb161c940f0f38bbbfa73e74f8a8ea5e83af
Moonsik Song <sikso1892@gmail.com>
2023-07-10 11:57:55 +0900

    edit boj 1789

--- 1789.py
+++ 1789.py
@@ -4,5 +4,12 @@ sys.stdin = open('input.txt', 'r')
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+s = int(input())
 
+acc = 0
+
+for i in range(1, s):
+    acc += i
+    if acc > s:
+        print(i-1)
+        break
