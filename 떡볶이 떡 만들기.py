7580a16edab03b5ad14434cb828f2d00b81cea22
Moonsik Song <sikso1892@gmail.com>
2023-10-01 15:56:40 +0900

    떡볶이 떡 만들기

--- 떡볶이 떡 만들기.py
+++ 떡볶이 떡 만들기.py
@@ -2,22 +2,26 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
 start = 0
-end = max(a)
+end = max(arr)
 
-result = 0
 while start <= end:
+    mid = (start+end)//2
     total = 0
-    mid = (start + end) // 2
-    for x in a:
-        if x > mid:
-            total += x - mid
-    if total < m:
-        end = mid - 1
-    else:
-        result = mid
+    
+    for num in arr:
+        if (num - mid) > 0:
+            total += (num - mid)
+    
+    if total > m:
         start = mid + 1
         
+    else:
+        result = mid
+        end = mid - 1
+        
 print(result)
+
+
